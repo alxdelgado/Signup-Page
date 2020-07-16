755cdf9a6a09ec722ab93c17af12bbe8b5f69914
Alex Delgado <delgadoalex@icloud.com>
2020-07-16 14:04:49 -0500

    added background radient && form validation

--- src/components/signup-page/signup-page.jsx
+++ src/components/signup-page/signup-page.jsx
@@ -1,21 +1,40 @@
 // import packages; 
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 
 // import material-ui components; 
-import { Container } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
 
 // import react-bootstrap components; 
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+
 
 // import custom styles; 
 import '../signup-page/signup-page.css';
 
 export default function SignUp_Page() {
+    function formValidation() {
+        const [validated, setValidated] = useState(false);
+
+        const handleSubmit = (event) => {
+            const form = event.currentTarget;
+            if(form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            setValidated(true);
+        }
+    }; 
+
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <React.Fragment>
-                <div className="signup-card">
-                    <h1 className="card-title">Hello World</h1>
+                <div className="signup-backdrop">
+                    <Container component="div" fixed="false" maxWidth='lg'>
+                        <Form controlId=''>
+
+                        </Form>
+                    </Container>
                 </div>
             </React.Fragment>
         </Suspense>
